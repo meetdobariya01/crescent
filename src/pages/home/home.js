122519ff838ebaf7540a0a7843922645fd350d0f
meetdobariya01 <meetdobariya87@gmail.com>
2026-01-30 14:56:56 +0530

    first commit

---
+++ src/pages/home/home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+
+const Home = () => {
+  return (
+    <div>
+      {/* Header Component */}
+      <Header />
+      home
+
+      {/* Footer Component */}
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
